1c3b57945f6e018808e481d2fe8302266fd7b64e
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-09-23 19:37:21 +0200

    queries

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -5,9 +5,72 @@ const knex = require("../database");
 // /api/meals	GET	Returns all meals
 
 router.get("/", async (request, response) => {
+  let query = knex("meal");
+
+  if (
+    "maxPrice" in request.query &&
+    typeof request.query.maxPrice === "string"
+  ) {
+    query = query.whereRaw("price <= ?", [request.query.maxPrice]);
+  }
+
+  if (request.query.availableReservations) {
+    query = query.join("reservation", "meal.id", "=", "reservation.meal_id");
+  }
+
+  if ("title" in request.query && typeof request.query.title === "string") {
+    query = query.whereRaw("title LIKE ?", [`%${request.query.title}%`]);
+  }
+
+  if (
+    "dateAfter" in request.query &&
+    typeof request.query.dateAfter === "string"
+  ) {
+    query = query.whereRaw("`when` > ?", [request.query.dateAfter]);
+  }
+
+  if (
+    "dateBefore" in request.query &&
+    typeof request.query.dateBefore === "string"
+  ) {
+    query = query.whereRaw("`when` > ?", [request.query.dateBefore]);
+  }
+
+  if ("limit" in request.query && typeof request.query.limit === "string") {
+    query = query.limit(`${request.query.limit}`);
+  }
+
+  if (request.query.sort_key && request.query.sort_dir) {
+    const sortAllowed = ["when", "max_reservations", "price"];
+    const orderBy = request.query.sort_key.toString();
+    let queryIncludesAllowedSort = sortAllowed.some((element) =>
+      orderBy.includes(element)
+    );
+    if (queryIncludesAllowedSort) {
+      query = query.orderByRaw(`${orderBy} ${request.query.sort_dir}`);
+    } else {
+      response
+        .status(404)
+        .send("Bad request, only when, max_reservations, price allowed");
+    }
+  }
+
+  if (request.query.sort_key && !request.query.sort_dir) {
+    const sortAllowed = ["when", "max_reservations", "price"];
+    const orderBy = request.query.sort_key.toString();
+    let queryIncludesAllowedSort = sortAllowed.some((element) =>
+      orderBy.includes(element)
+    );
+    if (queryIncludesAllowedSort) {
+      query = query.orderByRaw(orderBy);
+    } else {
+      response
+        .status(404)
+        .send("Bad request, only when, max_reservations, price allowed");
+    }
+  }
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meal").select("title");
+    const titles = await query;
     response.json(titles);
   } catch (error) {
     throw error;
@@ -86,6 +149,26 @@ router.delete("/:id", async (request, response) => {
       .where({ "meal.id": request.params.id })
       .del();
     response.json({ message: "Meal deleted", id: deletedMeal[0] });
+  } catch (error) {
+    throw error;
+  }
+});
+
+//WEEK 3
+//api/meals?maxPrice=90
+
+router.get("/", async (request, response) => {
+  let query = knex.select("*").from("meals");
+
+  if ("maxPrice" in request.query) {
+    const maxPrice = request.query.maxPrice.toString();
+    console.log(maxPrice);
+    query = query.whereRaw("meal.price <= ?", [maxPrice]);
+  }
+  try {
+    // knex syntax for selecting things. Look up the documentation for knex for further info
+    const titles = await query;
+    response.json(titles);
   } catch (error) {
     throw error;
   }
